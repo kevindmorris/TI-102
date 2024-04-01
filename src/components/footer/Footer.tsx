f09c56b8f33cb70492657619c0cc0efa3523f974
Morris, Kevin <Kevin.Morris@noblis.org>
2024-04-01 12:50:44 -0400

    Update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,34 @@
+import React from "react";
 import "./Footer.style.css";
+
+const links = [
+  { label: "React", href: "https://react.dev/" },
+  { label: "BlueprintJs", href: "https://blueprintjs.com/docs/" },
+  { label: "Axios", href: "https://axios-http.com/docs/intro" },
+  {
+    label: "react-json-view-lite",
+    href: "https://github.com/AnyRoad/react-json-view-lite",
+  },
+  { label: "Vite", href: "https://vitejs.dev/" },
+];
 
 export default function Footer() {
   return (
     <footer>
       <p>
         Made with 💖 by <a href="https://kevindmorris.vercel.app/">kevin</a>{" "}
-        using <a href="https://react.dev/">React</a> and{" "}
-        <a href="https://webpack.js.org/">Webpack</a>. Hosted with{" "}
-        <a href="https://pages.github.com/">GitHub Pages</a>. Version{" "}
+        using{" "}
+        {links.map((e, i) => (
+          <React.Fragment key={e.href}>
+            <a href={e.href}>{e.label}</a>
+            {i === links.length - 2
+              ? ", and "
+              : i === links.length - 1
+              ? ". "
+              : ", "}
+          </React.Fragment>
+        ))}{" "}
+        Hosted with <a href="https://vercel.com/">Vercel</a>. Version{" "}
         {__APP_VERSION__}.
       </p>
     </footer>
